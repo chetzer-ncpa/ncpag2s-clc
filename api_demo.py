f4f014b9bc2e349265be116483a4ac9dac0c61a0
Claus Hetzer <claus@olemiss.edu>
2024-10-16 18:44:58 +0000

    First pass at usable API

---
+++ api_demo.py
@@ -0,0 +1,69 @@
+from ncpa.g2scli import G2SClient
+import matplotlib.pyplot as plt
+from datetime import datetime, timezone
+import numpy as np
+from numpy import NZERO
+
+client = G2SClient()
+t = datetime(2024,10,15,0,tzinfo=timezone.utc)
+lat1 = 19.59
+lon1 = -155.89
+lat2 = 64.84
+lon2 = -147.72
+npts = 51
+
+# Get a point profile
+profile = client.point(time=t,latitude=lat1,longitude=lon1)
+
+# Some basic info
+print(' '.join((
+    f'Downloaded profile for [{profile.location.latitude},{profile.location.longitude}]',
+    f'at {profile.time.strftime("%Y-%m-%d %H:00:00")}'))
+    )
+z0 = profile.parameters["Z0"]
+print(f'Ground height is {z0.values[0]} {z0.units}')
+
+fig, ax = plt.subplots(1,5,sharey=True,figsize=(11,6))
+fig.subplots_adjust(hspace=0.05,wspace=0.05)
+Z = profile.parameters["Z"].values
+for i, param in enumerate(['T','U','V','P','R']):
+    if param in ('P','R'):
+        ax[i].semilogx(profile.parameters[param].values,Z)
+    else:
+        ax[i].plot(profile.parameters[param].values,Z)
+    ax[i].set_title(f'{param} [{profile.parameters[param].units}]')
+    ax[i].grid(linestyle='--')
+    ax[i].set_ylim(0,150.0)
+
+ax[0].set_ylabel('Altitude [km]')
+fig.suptitle(f'[{profile.location.latitude},{profile.location.longitude}] at {profile.time.strftime("%Y-%m-%d %H:00:00")}')
+plt.show()
+plt.close()
+
+# Get a line profile
+line = client.line(time=t,start=(lat1,lon1),end=(lat2,lon2),points=npts)
+nz = len( line.profiles[0].parameters['Z'].values )
+rmax = line.range_to(line.profiles[-1])
+
+Tslice = np.ndarray((nz,npts))
+Uslice = np.ndarray((nz,npts))
+Vslice = np.ndarray((nz,npts))
+for i, profile in enumerate(line.profiles):
+    Tslice[:,i] = profile.parameters['T'].values
+    Uslice[:,i] = profile.parameters['U'].values
+    Vslice[:,i] = profile.parameters['V'].values
+
+fig, (Tax, Uax, Vax) = plt.subplots(nrows=3, sharex=True, figsize=(12,6))
+Tax.imshow(Tslice, extent=[0,rmax,0,150], aspect='auto')
+Tax.set_title('Temperature [K]')
+Uax.imshow(Uslice, extent=[0,rmax,0,150], aspect='auto')
+Uax.set_title('U Winds [m/s]')
+Vax.imshow(Vslice, extent=[0,rmax,0,150], aspect='auto')
+Vax.set_title('V Winds [m/s]')
+plt.tight_layout()
+plt.show()
+    
+    
+    
+    
+     
--- ncpa/g2scli/__init__.py
+++ ncpa/g2scli/__init__.py
@@ -1,12 +1,18 @@
-import sys
-import os
-
-
-from ncpa.g2scli.settings import config
+from ncpa.g2scli.settings import config as cli_config
 from ncpa.g2scli.commands import ModularCommand
 from ncpa.g2scli.version import get_version
+from ncpa.g2scli.urls import format_url
+from ncpa.g2scli.requests import request_and_write
 
-from configparser import ConfigParser
+from ncpa.g2s import G2SProfileSet
+from ncpa.g2s.formatters import G2SProfileDecoder, G2SFormatterFactory
+
+import sys
+import os
+# from configparser import ConfigParser
+from datetime import datetime, timezone, timedelta
+from io import StringIO
+import json
 
 VERSION = (0, 2, 0, "beta", 0)
 APPS = ('g2scli')
@@ -17,3 +23,73 @@ def execute_from_command_line(argv=None):
     utility = ModularCommand(argv)
     utility.execute()
 
+class G2SClient:
+    def __init__(self,
+                 chunksize=cli_config['requests'].getint('chunksize',2048),
+                 encoding=cli_config['requests'].get('encoding','utf-8'),
+                 timeout=cli_config['requests'].getint('timeout',30),
+                 ):
+        self._chunksize = chunksize
+        self._encoding = encoding
+        self._timeout = timeout
+        
+    def _fetch_json(self,request_type,**kwargs):
+        url = format_url(request_type,
+                         outputformat='json',
+                         **kwargs)
+        holder = StringIO(initial_value='')
+        request_and_write(url, 
+                          out=holder, 
+                          timeout=self._timeout, 
+                          chunksize=self._chunksize, 
+                          encoding=self._encoding)
+        output = json.loads(holder.getvalue(),cls=G2SProfileDecoder)
+        holder.close()
+        return output
+        
+        
+    def point(self,time,latitude,longitude):
+        return self._fetch_json(request_type='point',time=time,latitude=latitude,longitude=longitude)
+        
+            
+        # url = format_url('point', 
+        #                  time=time, 
+        #                  latitude=latitude, 
+        #                  longitude=longitude,
+        #                  outputformat='json')
+        #
+        # holder = StringIO(initial_value='')
+        # request_and_write(url, 
+        #                   out=holder, 
+        #                   timeout=self._timeout, 
+        #                   chunksize=self._chunksize, 
+        #                   encoding=self._encoding)
+        # profile = json.loads(holder.getvalue(),cls=G2SProfileDecoder)
+        # holder.close()
+        # return profile
+    
+    def line(self,time,start,end,points):
+        return self._fetch_json(request_type='line',
+                                time=time,
+                                startlatitude=start[0],
+                                startlongitude=start[1],
+                                endlatitude=end[0],
+                                endlongitude=end[1],
+                                points=points
+                                )
+        
+    def grid(self,time,start,end,latpoints,lonpoints):
+        return self._fetch_json(request_type='grid',
+                                time=time,
+                                startlatitude=start[0],
+                                startlongitude=start[1],
+                                endlatitude=end[0],
+                                endlongitude=end[1],
+                                latpoints=latpoints,
+                                lonpoints=lonpoints
+                                )
+        
+    
+        
+        
+        
